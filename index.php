faadd9942f4a0190dc3e83464a29da35c8c7260f
nilesh230 <nilesh.k2313@gmail.com>
2018-10-21 09:39:55 -0400

    index file added by nilesh

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Resume - Start Bootstrap Theme</title>
+    <!-- Bootstrap core CSS -->
+    <link href="lib/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="lib/css/resume.min.css" rel="stylesheet">
+  </head>
+
+  <body id="page-top">
+	<?php
+	require './element/sidebar.php';
+	?>
+	<div class="container">
+		<h2>Title</h2>
+	</div>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    
+
+  </body>
+
+</html>
